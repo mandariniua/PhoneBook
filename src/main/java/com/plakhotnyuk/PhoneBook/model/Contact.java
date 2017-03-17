7e6e41e35ca28fa76461816cc4660b8ca9922a2c
mandariniua <mandarini.ua@gmail.com>
2017-03-17 23:14:13 +0200

    Added Contact class

--- src/main/java/com/plakhotnyuk/PhoneBook/model/Contact.java
+++ src/main/java/com/plakhotnyuk/PhoneBook/model/Contact.java
@@ -1,11 +1,63 @@
 package com.plakhotnyuk.PhoneBook.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name ="contact")
 public class Contact {
 	
-	private int id;
+	private long id;
 	private String secondName;
 	private String firstName;
 	private String otchestvo;
 	private int number;
+	private User user;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getSecondName() {
+		return secondName;
+	}
+	public void setSecondName(String secondName) {
+		this.secondName = secondName;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getOtchestvo() {
+		return otchestvo;
+	}
+	public void setOtchestvo(String otchestvo) {
+		this.otchestvo = otchestvo;
+	}
+	public int getNumber() {
+		return number;
+	}
+	public void setNumber(int number) {
+		this.number = number;
+	}
+	public User getUser() {
+		return user;
+	}
+	public void setUser(User user) {
+		this.user = user;
+	}
+	
+	
 
 }
+
+
