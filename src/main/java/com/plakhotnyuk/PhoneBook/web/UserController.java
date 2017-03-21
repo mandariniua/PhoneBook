a805b5665d202334dfa6fe87c09b7225acb87ba1
mandariniua <mandarini.ua@gmail.com>
2017-03-21 21:32:21 +0200

    Commit

--- src/main/java/com/plakhotnyuk/PhoneBook/web/UserController.java
+++ src/main/java/com/plakhotnyuk/PhoneBook/web/UserController.java
@@ -61,6 +61,5 @@ public class UserController {
     @RequestMapping(value = {"/", "/welcome"}, method = RequestMethod.GET)
     public String welcome(Model model) {
         return "welcome";
-        
     }
 }
